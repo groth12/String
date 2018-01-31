26d544818f0c4433025ae8b8d1ffb44772cc3c10
Danny Akimchuk <dakimchuk@ithaca.edu>
2018-01-31 09:35:40 -0500

    Fixed test for one number string. -dpa

--- test/edu/ithaca/roth/StringCalculatorimplTest.java
+++ test/edu/ithaca/roth/StringCalculatorimplTest.java
@@ -9,11 +9,18 @@ class StringCalculatorimplTest {
     void AddTest(){
         StringCalculator myFunctions = new StringCalculatorimpl();
         String numbers  = "1";
-        assertEquals("", numbers, "No input!");
+        int sum = myFunctions.Add(numbers);
+        assertEquals(1, sum, "Expected: 1 Actual: " + sum);
+
+        sum = myFunctions.Add("0");
+        assertEquals(0, sum, "Expected: 0 Actual: " + sum);
+
+        sum = myFunctions.Add("-1");
+        assertEquals(-1, sum, "Expected: -1 Actual: " + sum);
         /*
         int total = numbers.charAt(0) + numbers.charAt(1) + numbers.charAt(2);
         assertEquals(total, numbers.get(0) + numbers.get(1) + numbers.get(2), "Numbers not equal!");
        */
-        int total = myFunctions.Add(numbers);
+        //int total = myFunctions.Add(numbers);
     }
 }
